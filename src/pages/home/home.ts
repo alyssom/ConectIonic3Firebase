f8c961c06b33afb07c1251d150ec7b80dfc64e9b
Alyssom Falkenberg <alyssommartins@gmail.com>
2017-09-19 23:34:49 -0300

    inserido botão que chama a função que retorna localização atual do usuário. Ja em TypeScript.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,13 +8,16 @@ import { AngularFireDatabase } from 'angularfire2/database';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
+  //Array onde serao intupados os dados
   arrData = [];
   myInput;
   myInputDois;
 
+
+  // atributo fdb contem os dados do banco
   constructor(public navCtrl: NavController, private fdb: AngularFireDatabase) {
-      this.fdb.list("/myItems/").subscribe(_data => {
+    //lista o que tem no array  
+    this.fdb.list("/barbearias/la_mafia").subscribe(_data => {
         this.arrData = _data;
 
         console.log(this.arrData);
@@ -22,15 +25,34 @@ export class HomePage {
 
   }
 
- 
+  //inclui de acordo com o caminho.
   btnAddClickedDois(){
-    this.fdb.list("/Barbearias/LaMafia").push(this.myInputDois);
+    this.fdb.list("/barbearias/la_mafia").push(this.myInputDois);
   }
 
+  localizarUsuario(){
+    if (window.navigator && window.navigator.geolocation) {
+     var geolocation = window.navigator.geolocation;
+     geolocation.getCurrentPosition(sucesso, erro);
+    } else {
+       alert('Geolocalização não suportada em seu navegador.')
+    }
+    function sucesso(posicao){
+      console.log(posicao);
+      var latitude = posicao.coords.latitude;
+      var longitude = posicao.coords.longitude;
+      alert('Sua latitude estimada é: ' + latitude + ' e longitude: ' + longitude )
+    }
+    function erro(error){
+      console.log(error)
+    }
+  }
+
+
+  //deleta conforme o caminho com acao de clica
   delete(i){
     
-    this.fdb.list("/myItems/").remove(this.arrData[i].$key);
-    this.fdb.list("/myAlunos/SD1").remove(this.arrData[i].$key);
+    this.fdb.list("/barbearias/la_mafia").remove(this.arrData[i].$key);
     
   }
 }
