dc7e3c359a543dc965f59e765472dd8c11fc6386
Alyssom Falkenberg <alyssommartins@gmail.com>
2017-10-04 14:04:35 -0300

    Funcão com API do Matrix funcionando, retornando a distância  em KM de o ponto onde o usuário esta até o a Barbearia;

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,12 +4,13 @@ import { NavController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 
 
-
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+
 export class HomePage {
   //Array onde serao intupados os dados
   myInput;
@@ -31,13 +32,10 @@ export class HomePage {
       //   console.log(this.localizacaoBarbearia);
       // })
 }
-  
-
-       
-
 
 
   
+
   //inclui de acordo com o caminho.
   btnAddClickedDois(){
     this.fdb.list("/barbearias/la_mafia").push(this.myInputDois);
@@ -45,7 +43,6 @@ export class HomePage {
     
 
   }
-
 
   localizarUsuario(){
     
@@ -57,7 +54,6 @@ export class HomePage {
       localizacaoBarbearia = snapshot.val().localizacao;
       console.log(localizacaoBarbearia);
     })
-
 
     if (window.navigator && window.navigator.geolocation) {
      var geolocation = window.navigator.geolocation;
@@ -78,22 +74,93 @@ export class HomePage {
       "&destinations=" + localizacaoBarbearia +
       "&key=AIzaSyDl3kN1tvhtZMhXKy_zVmpnHmVty8PXYBg";
 
-     
-     
+      console.log(urlMaps)
+      
+      // var origem = latitude + "," + longitude;
+      // var service = new google.maps.DistanceMatrixService();
+      // service.getDistanceMatrix(
+      //   {
+      //     origins: [origem],
+      //     destinations: [localizacaoBarbearia],
+      //     travelMode: google.maps.TravelMode.DRIVING,
+      //     unitSystem: google.maps.UnitSystem.METRIC
+      //   }, callback);
+      
+      // function callback(response, status) {
+      //   // See Parsing the Results for
+      //   // the basics of a callback function.
+      // }
+
+
+    var distance = require('google-distance-matrix');
+      
+     var origins = [localizacaoBarbearia];
+     var destinations = [latitude + "," + longitude];
+      
+     distance.key('AIzaSyDl3kN1tvhtZMhXKy_zVmpnHmVty8PXYBg');
+     distance.units('metric');
+      
+     distance.matrix(origins, destinations, function (err, distances) {
+         if (err) {
+             return console.log(err);
+         }
+         if(!distances) {
+             return console.log('no distances');
+         }
+         if (distances.status == 'OK') {
+             for (var i=0; i < origins.length; i++) {
+                 for (var j = 0; j < destinations.length; j++) {
+                     var origin = distances.origin_addresses[i];
+                     var destination = distances.destination_addresses[j];
+                     if (distances.rows[0].elements[j].status == 'OK') {
+                         var distance = distances.rows[i].elements[j].distance.text;
+                         console.log('Distancia de ' + origin + ' até ' + destination + ' é ' + distance);
+                     } else {
+                         console.log(destination + ' is not reachable by land from ' + origin);
+                     }
+                 }
+             }
+         }
+     });
+
+
+
+
       // ------------------------------------------------------------------------------------------------
       //-------------------------NÃO ESTA FUNCIONANDO ESTA PARTE DO REQUEST------------------------------
       //-------------------------------------------------------------------------------------------------
+      // var directionsDisplay = new google.maps.DirectionsRenderer;
+      // var directionsService = new google.maps.DirectionsService;
+      // var map = new google.maps.Map(document.getElementById('map'), {
+      //   zoom: 7,
+      //   center: {lat: 41.85, lng: -87.65}
+      // });
+
+      // directionsDisplay.setMap(map);
+      // directionsService.route({
+      //   origin: latitude + "," + longitude,
+      //   destination: localizacaoBarbearia,
+      //   travelMode: 'DRIVING'
+      // })
       
-      // var xhttp = new XMLHttpRequest();
-      // xhttp.open("GET", urlMaps , true);
-      // xhttp.setRequestHeader("Content-type", "application/json");
-      // xhttp.send();
-      // var response = JSON.parse(xhttp.responseText);
+      // function calcularRouta(directionsService, directionsDisplay){
+      //   directionsService.route({
+      //     origin: document.getElementById('start').nodeValue,
+      //     destination: document.getElementById('end').nodeValue,
+      //     travelMode: 'DRIVING'
+      //   }),function (response, status) {
+      //     if(status === 'OK'){
+      //       directionsDisplay.setDirections(response);
+      //     }else{
+      //       window.alert(status);
+      //     }
+      //   }
+      // }
+      
 
-      // console.log(response)
 
-
-    }
+      }
+      
     function erro(error){
       console.log(error)
     }
